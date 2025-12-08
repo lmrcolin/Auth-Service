645d0aec1af2e650b3f7855c3374e196f346cb35
lmrcolin <luismanuelrc@ciencias.unam.mx>
2025-12-07 22:56:53 -0600

    Servicio actualizado.

--- src/main/java/com/unam/dwb/auth/repo/UsuarioJdbcRepository.java
+++ src/main/java/com/unam/dwb/auth/repo/UsuarioJdbcRepository.java
@@ -11,47 +11,43 @@ import com.unam.dwb.auth.domain.Usuario;
 
 @Repository
 public class UsuarioJdbcRepository {
-	
+
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
-	
+
 	public Optional<Usuario> findByUsername(String username) {
-		String sql = "SELECT username, correo FROM dwb.usuario WHERE username = ?";
-		
+		String sql = "SELECT username, correo FROM usuario WHERE username = ?";
+
 		List<Usuario> resultadoQuery = jdbcTemplate.query(sql,
 				(rs, row) -> new Usuario(
 						rs.getString("username"),
-						rs.getString("correo")
-						),
+						rs.getString("correo")),
 				username);
-		
+
 		return resultadoQuery.stream().findFirst();
 	}
-	
+
 	public Optional<Usuario> findByUsernameCredential(String username, String hashPass) {
-		String sql = "SELECT username, correo FROM dwb.usuario WHERE username = ? AND password = ?";
-		
+		String sql = "SELECT username, correo FROM usuario WHERE username = ? AND password = ?";
+
 		List<Usuario> resultadoQuery = jdbcTemplate.query(sql,
 				(rs, row) -> new Usuario(
 						rs.getString("username"),
-						rs.getString("correo")
-						),
+						rs.getString("correo")),
 				username, hashPass);
-		
+
 		return resultadoQuery.stream().findFirst();
 	}
-	
+
 	public Optional<Usuario> findByCorreoCredential(String correo, String hashPass) {
-		String sql = "SELECT username, correo FROM dwb.usuario WHERE correo = ? AND password = ?";
-		
+		String sql = "SELECT username, correo FROM usuario WHERE correo = ? AND password = ?";
+
 		List<Usuario> resultadoQuery = jdbcTemplate.query(sql,
 				(rs, row) -> new Usuario(
 						rs.getString("username"),
-						rs.getString("correo")
-						),
+						rs.getString("correo")),
 				correo, hashPass);
-		
+
 		return resultadoQuery.stream().findFirst();
 	}
 
